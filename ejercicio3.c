2b77d786f19d7a7a7db939ba9d9e15ea2fcbb474
MathiasContreras <mathias.contreras@udla.edu.ec>
2026-05-15 15:16:17 -0500

    Primer commit ejercicio 3

---
+++ ejercicio3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    char vocales[6]={'a','e','i','o','u'};
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%c ", vocales[6]);
+    }
+    
+}
