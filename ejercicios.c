548941a1c73c4ed39c685dbf0170ebf511ca14d1
MathiasContreras <mathias.contreras@udla.edu.ec>
2026-05-15 14:19:48 -0500

    Inclusion de funcion puntero

---
+++ ejercicios.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]){
+    int arreglo[]={3,6,9,12,15,18};
+    int *ptr;
+
+    ptr = &arreglo[0];
+    printf ("%p %d\t", ptr, *ptr);
+    
+    for (int i=0; i < 6; i++)
+    {
+        printf("%d", arreglo [i]);
+    }
+    
+    return 0;
+}
